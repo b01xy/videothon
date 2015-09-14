c505b28bb7d73d5f381292cdf0916e61413bd752
b01 <b01@db.ultimedia>
2015-09-14 20:29:05 +0200

    Premier commit, tout marche !

---
+++ README.md
@@ -0,0 +1,9 @@
+# videothon
+
+Ce système permet d'enregistrer des contributions en audio video en mode "VideoThon"
+
+Un simple bouton relié à la barre espace permet de passer d'une étape à l'autre
+
+De la documentation : http://wiki.labomedia.org/index.php/VideoThon_Pure_Data_Linux
+
+Licence : CC-BY-SA-NC 3.0
---
+++ launch_rec.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#ffmpeg -thread_queue_size 512 -f alsa -i hw:0,0  -thread_queue_size 512 -f video4linux2 -i /dev/video10 -c:v libx264 -preset fast -pix_fmt yuv420p -c:a aac -ab 320000 -ar 48000 -ac 2 -strict -2 -t $1 videos/VIDEO$(date +%Y-%m-%d-%H:%M).mkv
+ffmpeg -thread_queue_size 512 -f alsa -i hw:0,0  -thread_queue_size 512 -f video4linux2 -i /dev/video10 -c:v mjpeg -b:v 16M -c:a aac -ab 320000 -ar 48000 -ac 2 -strict -2 -t $1 videos/VIDEO$(date +%Y-%m-%d-%H:%M).mkv
+exit 0
